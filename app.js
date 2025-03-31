eec2fe9330a3891d875e1a3abaed06f765baa711
jonorl <39122230+jonorl@users.noreply.github.com>
2025-03-31 14:17:11 +0300

    Test successful

--- app.js
+++ app.js
@@ -1,31 +1,103 @@
-// Load .env with path, API keys and other secrets
-require("dotenv").config();
-
-// Load Express and path
-const express = require("express");
-const app = express();
 const path = require("node:path");
+const { Pool } = require("pg");
+const express = require("express");
+const session = require("express-session");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
-// Load Routers
-const mainRouter = require("./routes/mainRouter");
+const pool = new Pool({
+  host: "localhost",
+  user: "jonorl",
+  database: "authentication_test",
+  password: "Deutschland1",
+  port: 5432,
+});
 
-// Add this code to make it possible to render .ejs files
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query(
+        "SELECT * FROM users WHERE username = $1",
+        [username]
+      );
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        // passwords do not match!
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
+      id,
+    ]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// Add this code to make it possible to load css
-const assetsPath = path.join(__dirname, "public");
-app.use(express.static(assetsPath));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
-// Add this code to parse data to POST requests
-app.use(express.urlencoded({ extended: true }));
-
-// Set port to whatever is on .env otherwise use 3000
-const PORT = process.env.PORT || 3000;
-
-app.use("/", mainRouter);
-
-// Message in the console to confirm that the server launched correctly
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}!`);
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user });
 });
+
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+app.post("/sign-up", async (req, res, next) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    await pool.query("insert into users (username, password) values ($1, $2)", [
+      req.body.username,
+      hashedPassword,
+    ]);
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+
+app.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
