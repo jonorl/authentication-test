7ec304b7eb3bb19971190c7058c831229bb42501
jonorl <39122230+jonorl@users.noreply.github.com>
2025-04-02 11:58:22 +0300

    test

--- app.js
+++ app.js
@@ -100,4 +100,4 @@ app.get("/log-out", (req, res, next) => {
   });
 });
 
-app.listen(3000, () => console.log("app listening on port 3000!"));
+app.listen(3001, () => console.log("app listening on port 3001!"));
